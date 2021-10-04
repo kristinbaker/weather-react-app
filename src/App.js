c86fcb46d2c2f45a0b6ae530803babbe632016db
Kristin Baker <87988811+kristinbaker@users.noreply.github.com>
2021-10-04 14:14:22 -0500

    Added links and styled footer

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ export default function App() {
         <Forecast />
       </div>
       <div className="footer">
+        This project was coded by <a className="footer-links" href="https://www.linkedin.com/in/kristinbaker1/" alt="Personal Website"> Kristin Baker </a> and is  {""}
         <a
-          className="code-link"
+          className="footer-links"
           href="https://github.com/kristinbaker/weather-react-app"
           alt="Github Link"
         >
-          Open-source code{" "}
-        </a>{" "}
-        by Kristin Baker
+          open-sourced on GitHub {" "}
+        </a>{" "} and {""}
+        <a className="footer-links" href="https://competent-panini-20a53a.netlify.app/" alt="Netlify link"> hosted on Netlify</a>
       </div>
     </div>
   );
